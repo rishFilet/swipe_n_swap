a671fc6fd4e25ed0f846d71d0088ef825777f642
Jonathan Oliveira <jonathan.oliveira@ecobee.com>
2020-09-11 17:34:28 -0400

    initial display of elements in screen

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -2,22 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:swipe_swap/utilities/constants.dart';
 
 class LoginPage extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Constants.kAppTitle),
-      ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(
+            Constants.kDefaultPadding,
+            Constants.kDefaultPadding * 4,
+            Constants.kDefaultPadding,
+            0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              'This is where the login should go',
+              Constants.kAppTitle,
+            ),
+            Text(
+              'Log in to find the best trades',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  onPressed: null,
+                  child: Text('Google'),
+                ),
+                FlatButton(
+                  onPressed: null,
+                  child: Text('Facebook'),
+                ),
+              ],
+            ),
+            Text(
+              'Or log in with Email',
+            ),
+            Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter your email address',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    validator: (value) =>
+                        !value.contains('@') ? "Not a valid email" : null,
+                    onSaved: (_) => null, // will send event to Bloc
+                  ),
+                  SizedBox(height: Constants.kDefaultPadding),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter your password',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    validator: (value) => value.length < 6
+                        ? "Must be at least 6 characters long"
+                        : null,
+                    onSaved: (_) => null,
+                    obscureText: true, // will send event to Bloc
+                  ),
+                ],
+              ),
+            ),
+            FlatButton(
+              onPressed: _manualLogin,
+              child: Text('Log in'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _manualLogin() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+    }
   }
 }
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,3 +1,10 @@
 class Constants {
+
+  // MARK: Strings
+
   static const kAppTitle = "Swap n Swipe";
+
+  // MARK: doubles
+
+  static const kDefaultPadding = 16.0;
 }
