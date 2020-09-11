494e731287c92ad4e044d7e77f88666c1feada8f
Jonathan Oliveira <jonathan.oliveira@ecobee.com>
2020-09-11 17:39:32 -0400

    Locks the app into Portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:swipe_swap/features/login/presentation/pages/login_page.dart';
 
 void main() {
@@ -9,6 +10,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Prevents the app from changing orientation
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+
     return MaterialApp(
       title: 'Swipe n Swap',
       debugShowCheckedModeBanner: false,
